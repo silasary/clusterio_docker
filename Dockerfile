ba2d510855fffc7e76a2e45cc39baa5d1ed11f27
Katelyn Gigante <clockwork.singularity@gmail.com>
2026-04-05 18:01:11 +1000

    Alpine can't run the server

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
-FROM node:20-alpine
-RUN apk add --no-cache curl
+FROM node:24
+RUN apt-get update && apt-get install -y curl
 
-RUN adduser -D clusterio
+RUN adduser --disabled-password clusterio
 RUN mkdir /clusterio && chown clusterio:clusterio /clusterio && mkdir /factorio && chown clusterio:clusterio /factorio
+# RUN ln -s /lib /lib64
 USER clusterio
 
 WORKDIR /clusterio
@@ -12,6 +13,11 @@ COPY docker-entrypoint.sh /docker-entrypoint.sh
 ENV CLUSTERIO_MODE=standalone
 ENV CLUSTERIO_ADMIN=
 ENV CLUSTERIO_PLUGINS="@clusterio/plugin-global_chat @clusterio/plugin-inventory_sync @clusterio/plugin-player_auth @clusterio/plugin-research_sync @clusterio/plugin-statistics_exporter @clusterio/plugin-subspace_storage @hornwitser/discord_bridge @hornwitser/server_select"
+
+ENV CLUSTERIO_HOST_CONTROLLER_TOKEN=
+ENV CLUSTERIO_HOST_CONTROLLER_URL=http://localhost:8080/
+ENV CLUSTERIO_HOST_NAME=Host
+
 
 CMD [ "/bin/sh", "/docker-entrypoint.sh" ]
 # CMD [ "/bin/sh", "-c", "while true; do sleep 1; done" ]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,4 @@
-if [ "$CLUSTERIO_MODE" = "host" ] || [ "$CLUSTERIO_MODE" = "standalone" ]; then
+if [ ! -d /factorio/factorio ] && ([ "$CLUSTERIO_MODE" = "host" ] || [ "$CLUSTERIO_MODE" = "standalone" ]); then
     wget -q -O factorio.tar.gz https://www.factorio.com/get-download/latest/headless/linux64 && tar -xf factorio.tar.gz && rm factorio.tar.gz && mv factorio /factorio
 fi
 if [ "$CLUSTERIO_MODE" = "controller" ] || [ "$CLUSTERIO_MODE" = "standalone" ]; then
@@ -12,7 +12,7 @@ if [ "$CLUSTERIO_MODE" = "controller" ] || [ "$CLUSTERIO_MODE" = "standalone" ];
     /bin/sh ./run-controller.sh
 elif [ "$CLUSTERIO_MODE" = "host" ]; then
     if [ ! -f /clusterio/config-host.json ]; then
-        npm init "@clusterio" -- --mode host
+        npm init "@clusterio" -- --mode host --controller-token $CLUSTERIO_HOST_CONTROLLER_TOKEN --controllerUrl $CLUSTERIO_HOST_CONTROLLER_URL --hostName $CLUSTERIO_HOST_NAME --no-download-headless --factorio-dir /factorio/factorio --remoteNpm --plugins $CLUSTERIO_PLUGINS
     fi
     /bin/sh ./run-host.sh
 else
